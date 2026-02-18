bec5c819e97589e9e1c6c2ae74afe2e55b30931d
Mateus-Isaque <mateusisaque2007@gmail.com>
2026-02-17 21:47:27 -0300

    Desafio completo da DIO

---
+++ Script/index.js
@@ -0,0 +1,20 @@
+let nome = "Mateus"
+let xp = 100000
+
+if (xp <= 1000){
+    console.log(`O Herói de noome ${nome} está no nível de Ferro!`);
+} else if (xp <= 2000){
+    console.log(`O Herói de noome ${nome} está no nível de Bronze!`);
+} else if (xp <= 5000){
+    console.log(`O Herói de noome ${nome} está no nível de Prata!`);
+} else if (xp <= 7000){
+    console.log(`O Herói de noome ${nome} está no nível de Ouro!`);
+} else if (xp <= 8000){
+    console.log(`O Herói de noome ${nome} está no nível de Platina!`);
+} else if (xp <= 9000){
+    console.log(`O Herói de noome ${nome} está no nível de Ascendente!`);
+} else if (xp <= 10000){
+    console.log(`O Herói de noome ${nome} está no nível de Imortal!`);
+} else {
+    console.log(`O Herói de noome ${nome} está no nível de Radiante!`);
+}
